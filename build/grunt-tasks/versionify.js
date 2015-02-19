2ad64eb7adfaaced73608c3ef5f745ebdd76c09d
Brian Feister <brian.feister@scala.com>
2015-02-19 17:49:34 -0500

    Add direct links to github commits and releases in version meta

--- build/grunt-tasks/versionify.js
+++ build/grunt-tasks/versionify.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
       var fileContents = grunt.file.read( filePath );
       var timestamp = grunt.template.today('UTC: yyyy-mm-dd HH:MM:ss Z');
       var fileContents = fileContents.replace( /<!-- release\[(.*?)\] -->/ig, '<!-- release[ time:'+timestamp+' | version: '+pkg.version+' | git SHA: '+git.short()+' ] -->');
-      var fileContents = fileContents.replace( /window\.expV(.*?)\/\/expV/ig, 'window.expV = { version: \''+pkg.version+'\', sha: \''+git.short()+'\', timestamp: \''+timestamp+'\' } //expV');
+      var fileContents = fileContents.replace( /window\.expV(.*?)\/\/expV/ig, 'window.expV = { version: \''+pkg.version+'\', gitUrl: \''+pkg.repository.url+'\', sha: \''+git.short()+'\', timestamp: \''+timestamp+'\' } //expV');
       grunt.file.write( filePath, fileContents );
   });
 };
